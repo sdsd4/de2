dbba56d85967a89ec34bce0e695c7354f3eeef30
sd <sdbubenchikov@gmail.com>
2026-06-01 17:12:08 +0300

    final 3

--- app_check.py
+++ app_check.py
@@ -21,4 +21,4 @@ def index():
     return f"Сайт ок<p>{status}</p>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
